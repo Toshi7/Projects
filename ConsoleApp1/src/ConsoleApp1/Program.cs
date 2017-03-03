f83dc6f791f589129936685c993f142bbf32015b
Toshi7 <ttoshi0719@gmail.com>
2017-03-03 17:19:19 +0900

    Add project files.

---
+++ ConsoleApp1/src/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("It's a brave new cross-platform world!");
+            Console.ReadLine();
+        }
+    }
+}
